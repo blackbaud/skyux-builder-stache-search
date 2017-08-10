1eb2774ef347165d58d56c6f6c63f75b706e1f49
blackbaud-brandonhare <brandon.hare@blackbaud.com>
2017-08-10 13:40:01 -0400

    Initial commit

---
+++ src/search.js
@@ -0,0 +1,146 @@
+const path = require('path');
+const fs = require('fs');
+const config = require('../skyuxconfig.json');
+
+const template = `// Use browser to access other sites (that are running angular)
+import { browser, element, by } from 'protractor';
+
+// Use SkyHostBrowser to access your locally served SPA
+import { SkyHostBrowser } from '@blackbaud/skyux-builder/runtime/testing/e2e';
+
+const fs = require('fs');
+const path = require('path');
+
+const walkSync = (dir, filePaths: string[] = []) => {
+  let files = fs.readdirSync(dir);
+  files.forEach(file => {
+    if (fs.statSync(path.join(dir, file)).isDirectory()) {
+      filePaths = walkSync(path.join(dir, file), filePaths);
+    } else {
+      if (file.includes('index.html')) {
+        filePaths.push(path.join(dir, file));
+      }
+    }
+  });
+  return filePaths;
+};
+
+describe('Search Results', () => {
+  let files;
+
+  function removeUnnecessaryElements() {
+    Array.from(
+      document.querySelectorAll(
+        '.stache-sidebar, .stache-breadcrumbs, .stache-table-of-contents'
+      )
+    ).forEach(el => el.remove());
+  }
+
+  beforeEach(() => {
+    jasmine.DEFAULT_TIMEOUT_INTERVAL = 500000;
+    files = walkSync(path.join(__dirname, '..', 'src', 'app'));
+    files = files.map(file => {
+      let route = file.split('/app')[1];
+      route = route.slice(0, route.length - 11);
+      if (route === '') {
+        route = '/';
+      }
+      return route;
+    });
+  });
+
+  it('should generate search results', (done) => {
+    let config = JSON.parse(browser.params.skyPagesConfig);
+    let appName = config.skyux.name;
+    let url = config.skyux.host.url;
+    let content = {
+      name: appName,
+      url: url
+    };
+
+    function writeSearchFile(searchDirPath) {
+      return new Promise((resolve, reject) => {
+        fs.writeFile(
+          path.join(searchDirPath, 'search.json'),
+          JSON.stringify(content),
+          (err) => {
+            err ? reject(err) : resolve();
+          }
+        );
+      });
+    }
+
+    function scrapePageContent(file: string) {
+      let pageContent = { path: file };
+      return SkyHostBrowser
+        .get(file)
+        .then(() => {
+          return browser.executeScript(removeUnnecessaryElements);
+        })
+        .then(() => {
+            return element(by.css('.stache-wrapper')).getText();
+        })
+        .then(text => {
+          pageContent['text'] = text;
+          return element(by.css('.stache-page-title')).getText();
+        })
+        .then(text => {
+          pageContent['title'] = text;
+          return pageContent;
+        })
+        .catch(error => {
+          if (error.name === 'NoSuchElementError') {
+            console.log('Must have the <stache> tag and a pageTitle on page to scrape content.');
+            return pageContent;
+          } else {
+            throw new Error(error);
+          }
+        });
+    }
+
+    Promise.all(files.map(file => {
+      return scrapePageContent(file);
+    }))
+      .then(pageContents => {
+        let searchDirPath = path.join(
+          __dirname,
+          '..',
+          'src',
+          'stache',
+          'search'
+        );
+
+        content['content'] = pageContents;
+
+        if (!fs.existsSync(searchDirPath)) {
+          fs.mkdirSync(searchDirPath);
+          return writeSearchFile(searchDirPath);
+        }
+        return writeSearchFile(searchDirPath);
+      })
+      .then(() => {
+        done();
+      })
+      .catch(error => {
+        console.log('ERROR', error);
+        expect(error).toBeNull();
+        done();
+      });
+  });
+});
+`
+
+function addSearchSpecToProject() {
+    try {
+        if (!fs.existsSync('e2e')) {
+            fs.mkdirSync('e2e');
+        }
+        fs.writeFileSync(path.join('e2e', 'stache-search.e2e-spec.ts'), template);
+    } catch (error) {
+        throw new Error('[ERROR]: Unable to add stache search template to e2e directory.');
+    }
+}
+
+if (config.appSettings.search) {
+    addSearchSpecToProject();
+}
