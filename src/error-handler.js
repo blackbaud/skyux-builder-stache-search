c3adabdb2b84ba58d1ced80d1b1df04cb907357a
blackbaud-brandonhare <brandon.hare@blackbaud.com>
2017-09-12 17:12:47 -0400

    Added exit code for error handler.

--- src/error-handler.js
+++ src/error-handler.js
@@ -2,6 +2,7 @@ function errorHandler(err, config) {
     console.error(err);
     require('./remove-search-spec')(null, config);
     require('./remove-search-json')(null, config);
+    process.exit(1);
 }
 
 module.exports = errorHandler;
