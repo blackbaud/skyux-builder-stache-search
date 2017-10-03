bca95981b9db4c4cad314dd77d90d147763f4a18
blackbaud-brandonhare <brandon.hare@blackbaud.com>
2017-10-03 08:58:50 -0400

    Updated tests

--- src/publish-search.spec.js
+++ src/publish-search.spec.js
@@ -2,9 +2,13 @@
 
 const mock = require('mock-require');
 const fs = require('fs-extra');
+const btoa = require('btoa');
+const EventEmitter = require('events').EventEmitter;
 
 describe('Publish Search', () => {
+  let emitter;
   let publishSearch;
+
   const config = {
     appSettings: {
       stache: {
@@ -14,8 +18,10 @@ describe('Publish Search', () => {
       }
     }
   };
-  
-  beforeEach(() => {  
+
+  beforeEach(() => {
+    emitter = new EventEmitter();
+
     mock('./error-handler', function (error) {
       console.log(error);
     });
@@ -37,6 +43,7 @@ describe('Publish Search', () => {
       console.log(options.headers.Authorization);
       console.log(options.body);
       console.log(options.uri);
+      return emitter;
     });
 
     mock('path', {
@@ -46,7 +53,7 @@ describe('Publish Search', () => {
       }
     });
     process.env.endpoint = "https://localhost:5000/publisher";
-    process.env.audienceId = "AudienceId";
+    process.env.audienceId = "Audience\\Id";
     process.env.clientUserName = "t01\\example";
     process.env.clientKey = "apassword";
     publishSearch = mock.reRequire('./publish-search');
@@ -115,22 +122,53 @@ describe('Publish Search', () => {
   });
 
   it('should post the json file to the database', () => {
+    let authCredentials = btoa(`${process.env.clientUserName}:${process.env.clientKey}`);
     spyOn(console, 'log');
     publishSearch([], undefined);
+    emitter.emit('data', new Buffer(JSON.stringify({
+      access_token: 'test'
+    })));
+    emitter.emit('end');
+    emitter.emit('response', {
+      statusCode: 200
+    });
     expect(console.log).toHaveBeenCalledWith(process.env.endpoint);
-    expect(console.log).toHaveBeenCalledWith(`Bearer ${process.env.token}`);
+    expect(console.log).toHaveBeenCalledWith(`https://service-authorization.sky.blackbaud.com/oauth2/token?grant_type=client_credentials&audience_id=${encodeURIComponent(process.env.audienceId)}`);
+    expect(console.log).toHaveBeenCalledWith('Bearer test');
+    expect(console.log).toHaveBeenCalledWith(`Basic ${authCredentials}`);
     expect(console.log).toHaveBeenCalledWith(JSON.stringify({ test: "Some Example JSON" }));
     expect(console.log).toHaveBeenCalledWith('200: Search data successfully posted!');
   });
 
-  it('should throw an error if file post unsuccessful', () => {
-    mock('request', function (options, callback) {
-      callback({message: 'ERROR!'});
-    });
-    publishSearch = mock.reRequire('./publish-search');
+  it('should throw an error if the authentication post was unsuccessful', () => {
     spyOn(console, 'log');
     publishSearch([], undefined);
-    expect(console.log).toHaveBeenCalledWith(new Error('[ERROR]: Unable to post search data! ERROR!'));
+    emitter.emit('error', new Error('Unable to authenticate'));
+    expect(console.log).toHaveBeenCalledWith(new Error(`[ERROR]: Unable to retrieve SAS JWT! Unable to authenticate`));
+  });
+
+  it('should throw an error if the file post was unsuccessful', () => {
+    spyOn(console, 'log');
+    publishSearch([], undefined);
+    emitter.emit('data', new Buffer(JSON.stringify({
+      access_token: 'test'
+    })));
+    emitter.emit('end');
+    emitter.emit('error', new Error('Unable to post search'));
+    expect(console.log).toHaveBeenCalledWith(new Error(`[ERROR]: Unable to post search data! Unable to post search`));
+  });
+
+  it('should throw an error if the status code on post !== 200', () => {
+    spyOn(console, 'log');
+    publishSearch([], undefined);
+    emitter.emit('data', new Buffer(JSON.stringify({
+      access_token: 'test'
+    })));
+    emitter.emit('end');
+    emitter.emit('response', {
+      statusCode: 500
+    });
+    expect(console.log).toHaveBeenCalledWith(new Error(`[ERROR]: Unable to post search data! 500`));
   });
 
   it('should throw an error if unable to read search json file', () => {
@@ -146,6 +184,10 @@ describe('Publish Search', () => {
     publishSearch = mock.reRequire('./publish-search');
     spyOn(console, 'log');
     publishSearch([], undefined);
+    emitter.emit('data', new Buffer(JSON.stringify({
+      access_token: 'test'
+    })));
+    emitter.emit('end');
     expect(console.log).toHaveBeenCalledWith(new Error('[ERROR]: Unable to read search file at ./src/stache/search.json! It is broken!'));
   });
 
